c373b9e60256c6ec2d9166394d1d4e95b50cc475
Ayyrun <113419228+arun1000lol@users.noreply.github.com>
2025-05-04 10:25:47 +0200

    added Home page + routing

--- Jeckaverse/src/pages/Home.tsx
+++ Jeckaverse/src/pages/Home.tsx
@@ -3,7 +3,7 @@ const Home = () => {
 
     return (
         <div className="home">
-        <h1>Test home page</h1>
+        <h1>Test new page</h1>
         <p>i love jeckas voice!</p>
         </div>
     );
